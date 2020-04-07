7e75c744333465713f4a1df5dadf405b512a9e59
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2020-04-07 18:36:33 +0530

    Added display_notes function

--- atm.h
+++ atm.h
@@ -18,4 +18,12 @@
    2590 0x11012000
   31999 0xf3412014
 */
+
+#define TOTAL_NOTES 8
+#define DENOMINATIONS                \
+  {                                  \
+    2000, 500, 100, 50, 20, 10, 5, 1 \
+  }
+
 unsigned int get_money(unsigned short int);
+void display_notes(unsigned int);
--- main.c
+++ main.c
@@ -3,11 +3,12 @@
 
 int main(void)
 {
-  unsigned short int amounts[] = {1, 10, 11, 12, 32000, 39, 589, 2590, 31999};
-  for (int i = 0; i < 9; i++)
+  unsigned short int amounts[] = {1, 10, 39, 589, 2590, 31999};
+  for (int i = 0; i < 6; i++)
   {
     unsigned int notes = get_money(amounts[i]);
-    printf("%5u %08x\n", amounts[i], notes);
+    printf("For %u rupee:\n", amounts[i]);
+    display_notes(notes);
   }
 
   return 0;
