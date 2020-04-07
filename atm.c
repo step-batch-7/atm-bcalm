e03d5f3b15899b2a94d5b6c69497b21748426dcd
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2020-04-07 18:36:33 +0530

    Added display_notes function

--- atm.c
+++ atm.c
@@ -1,20 +1,36 @@
+#include <stdio.h>
 #include "atm.h"
 
 unsigned int get_money(unsigned short int amount)
 {
   unsigned short int remaining_balance = amount;
-  unsigned int result = 0x00000000;
-  int notes[8] = {2000, 500, 100, 50, 20, 10, 5, 1};
-  int total_notes = 8;
+  unsigned int notes = 0x00000000;
+  int denoms[] = DENOMINATIONS;
   int max_amount_limit = 31999;
   if (remaining_balance <= max_amount_limit)
   {
-    for (int idx = 0; idx < total_notes; idx++)
+    for (int idx = 0; idx < TOTAL_NOTES; idx++)
     {
-      result <<= 4;
-      result |= remaining_balance / notes[idx];
-      remaining_balance %= notes[idx];
+      notes <<= 4;
+      notes |= remaining_balance / denoms[idx];
+      remaining_balance %= denoms[idx];
     }
   }
-  return result;
+  return notes;
+}
+
+void display_notes(unsigned int notes)
+{
+  int denoms[] = DENOMINATIONS;
+  unsigned int divisor = 0x10000000;
+  for (int idx = 0; idx < TOTAL_NOTES; idx++)
+  {
+    int note_count = notes / divisor;
+    if (note_count)
+    {
+      printf("   %d %s of Rs %d\n", note_count, note_count > 1 ? "notes" : "note", denoms[idx]);
+    }
+    notes = notes % divisor;
+    divisor = divisor >> 4;
+  }
 }
